e1027cf283c7636ccaf5bfe436b48b8d43f37cc3
Márcio Sobel <marcinhosobel@hotmail.com>
2023-11-16 05:33:17 -0300

    Fix comment

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-    $('.theme-button').on('click', () => {
-        
+    
+    $('.theme-button').on('click', () => { 
         // change theme
         let css = $(':root'),
             theme = css.attr('data-theme') == 'dark' ? 'light' : 'dark';
             css.attr('data-theme', theme);
 
-        // change button label
+        // change button icon
         $('.theme-button').children().toggleClass('fa-moon fa-sun');
     });
 
-    // update theme
+    // get theme from browser settings
     let userPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
     if (userPrefersDark) {
         $('.theme-button').trigger('click');
